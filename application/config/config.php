ea40baf6557bb541b7cab65e46e5a31b519b6455
Jefferson Lam <jlam520@gmail.com>
2013-12-20 10:56:54 -0800

    Set up configuration files for project

--- application/config/config.php
+++ application/config/config.php
@@ -26,7 +26,7 @@ $config['base_url']	= '';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
@@ -224,7 +224,7 @@ $config['cache_path'] = '';
 | MUST set an encryption key.  See the user guide for info.
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = '08M04HfgS82S6TJ3Q8r24CLXfXFE32SY';
 
 /*
 |--------------------------------------------------------------------------
